7a489b205d12c8a89a31feb5e37a8d4983e3f480
Konstantin <k.elfimow@gmail.com>
2022-11-07 00:20:47 +0300

    Добавил комментарии, добавил print'ов, исправил вывод в output.txt, отрефакторил повторяющиеся строки

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ alphabet: list = list('abcdefg')
 
 
 def entropy(l_p: list) -> np.float64:
+    """ Энтропия """
     H: np.float64 = np.float64(0.0)
+
+    s1: str = ''
+    s2: str = ''
     for value in l_p:
-        H += np.float64(value) * np.log2(np.float64(value))
+        value = np.float64(value)
+
+        H += value * np.log2(value)
+
+        s1 += f' - {value} * log2({value})'
+        s2 += format(-value * np.log2(value), '.5g') + ' + '
+
     H = np.float64(-H)
+
+    print(s1, ' =\n', s2, ' = ', H)
+    print()
+
     return np.float64(format(H, '.8g'))
 
 
 def transpose_l_dict(lst: list) -> list:
+    """ Транспонирование списка словарей """
     transposed_l_dict: list = list()
     for i in range(len(lst)):
         transposed_l_dict.append(dict())
@@ -36,9 +51,8 @@ def find_full_conditional_p(list_ensembles_conditional_p: list, dict_ensemble_p:
         ensemble: dict = dict()
 
         for (key1, value1), (key2, value2) in zip(transposed_l_dict[i].items(), dict_ensemble_p.items()):
-
             p = np.float64(format(np.float64(value1) * np.float64(value2), '.5g'))
-            ensemble[f'{key2}{key1}'] = p
+            ensemble[f"{key2}{str(key1).split('|')[0]}"] = p
 
             print(f'p(xi = {key2}, xi+1 = {alphabet[i]}) = p({key2}) * p({key1}) = {value2} * {value1} = {p}')
 
@@ -49,50 +63,59 @@ def find_full_conditional_p(list_ensembles_conditional_p: list, dict_ensemble_p:
 
 
 def make_matrix_from_l_dict(l_dic: list) -> np.array:
+    """ Переход к шагу:
+    p(x_i+1 = a) = p(a|a) * p(x_i = a) + p(a|b) * p(x_i = b)
+    ...
+    p(a) + p(b) + ... = 1
+
+    ...
+    Создаём матрицу коэффициентов размером: (n-1) x n.
+    Для применения метода Гаусса потом. """
     matrix_zip: list = list()
     for i in range(len(l_dic)):
 
         l_i: list = list()
         for count, element in enumerate(l_dic[i].values()):
             if count == i:
+                # Сразу переносим коэфы из левой части
                 l_i.append(element - 1)
             else:
                 l_i.append(element)
         matrix_zip.append(l_i)
 
     zipped_rows = zip(*matrix_zip)
+    result_matrix: list = [list(row) for row in zipped_rows]
 
-    transpose_matrix: list = [list(row) for row in zipped_rows]
-    for row in transpose_matrix:
+    for row in result_matrix:
         row.append(0)
 
-    for j in range(len(transpose_matrix[0])):
-        transpose_matrix[len(transpose_matrix) - 1][j] -= 1
+    for j in range(len(result_matrix[0])):
+        result_matrix[len(result_matrix) - 1][j] -= 1
 
-    return np.array(transpose_matrix)
+    return np.array(result_matrix)
 
 
-def gaussPivotFunc(matrix: np.array) -> np.array:
-    copy_matrix = np.copy(matrix)
-    for nrow in range(len(copy_matrix)):
+def gauss_pivot_func(matrix: np.array) -> np.array:
+    result_matrix = np.copy(matrix)
+    for nrow in range(len(result_matrix)):
         # nrow равен номеру строки
         # np.argmax возвращает номер строки с максимальным элементом в уменьшенной матрице
         # которая начинается со строки nrow. Поэтому нужно прибавить nrow к результату
-        pivot = nrow + np.argmax(abs(copy_matrix[nrow:, nrow]))
+        pivot = nrow + np.argmax(abs(result_matrix[nrow:, nrow]))
         if pivot != nrow:
             # swap
             # matrix[nrow], matrix[pivot] = matrix[pivot], matrix[nrow] - не работает.
             # нужно переставлять строки именно так, как написано ниже
-            copy_matrix[[nrow, pivot]] = copy_matrix[[pivot, nrow]]
-        row = copy_matrix[nrow]
+            result_matrix[[nrow, pivot]] = result_matrix[[pivot, nrow]]
+        row = result_matrix[nrow]
         divider = row[nrow]  # диагональный элемент
         if abs(divider) < 1e-10:
             # почти нуль на диагонали. Продолжать не имеет смысла, результат счёта неустойчив
-            raise ValueError(f'Матрица несовместна. Максимальный элемент в столбце {nrow}: {divider:.3g}')
+            raise ValueError(f'Матрица несовместна. Максимальный элемент в столбце {nrow}: {divider:.5g}')
         # делим на диагональный элемент.
         row /= divider
         # теперь надо вычесть приведённую строку из всех нижележащих строчек
-        for lower_row in copy_matrix[nrow + 1:]:
+        for lower_row in result_matrix[nrow + 1:]:
             factor = lower_row[nrow]  # элемент строки в колонке nrow
             lower_row -= factor * row  # вычитаем, чтобы получить ноль в колонке nrow
 
@@ -105,74 +128,59 @@ def gaussPivotFunc(matrix: np.array) -> np.array:
                 factor = upper_row[nrow]
                 upper_row -= factor * row
         return copy_matrix
-    make_identity(copy_matrix)
 
-    return copy_matrix
+    make_identity(result_matrix)
+
+    return result_matrix
 
 
 def markov_chain(list_ensembles_conditional_p: list) -> list:
     print('Вход: \n', list_ensembles_conditional_p)
     print()
 
+    # Матрица коэффов
     matrix = make_matrix_from_l_dict(list_ensembles_conditional_p)
     print('Matrix: \n', matrix)
     print()
 
-    I_matrix = gaussPivotFunc(matrix)
+    # Единичная матрица решений
+    I_matrix = gauss_pivot_func(matrix)
     print('I_matrix: \n', I_matrix)
     print()
 
+    # p(a), p(b), p(c), ...
     dict_ensemble_p: dict = dict()
-
     for i in range(len(I_matrix)):
         dict_ensemble_p[alphabet[i]] = format(I_matrix[i][len(I_matrix[0]) - 1], '.5g')
 
     print('p(a), p(b), p(c), ...\n', dict_ensemble_p)
     print()
 
+    # [{p(x_i = a|x_i+1 = a), p(x_i = a|x_i+1 = b)},
+    # {p(x_i = b|x_i+1 = a), p(x_i = b|x_i+1 = b)}]
     result = find_full_conditional_p(list_ensembles_conditional_p, dict_ensemble_p)
 
+    print('H_xi = ')
     p: list = list()
     for p_i in dict_ensemble_p.values():
         p.append(p_i)
-    print(p)
     H_xi = entropy(p)
-    s1: str = ''
-    s2: str = ''
-    for value in dict_ensemble_p.values():
-        value = np.float64(value)
-        s1 += f'- {value} * log2({value})'
-        s2 += format(-value * np.log2(value), '.5g')
-        s2 += ' + '
-    s1 += '= \n'
-    s2 += '= '
-    print(s1, s2, H_xi)
-    print()
 
+    print('H_xi(x_i+1) = ')
     full_p: list = list()
-    for l in result:
-        full_p += list(l.values())
+    for lp_i in result:
+        full_p += list(lp_i.values())
     H_xi_xi_1 = entropy(full_p)
-    s3: str = ''
-    s4: str = ''
-    for value in full_p:
-        value = np.float64(value)
-        s3 += f'- {value} * log2({value})'
-        s4 += format(-value * np.log2(value), '.5g')
-        s4 += ' + '
-    s3 += '= \n'
-    s4 += '= '
-    print(s3, s4, H_xi_xi_1)
-    print()
 
     H_suffix_xi_xi_1 = np.float64(format(H_xi_xi_1 - H_xi, '.8g'))
-    print(f'Hxi(xi+1) = H(xixi+1) - H(xi) = {H_xi_xi_1} - {H_xi} = {H_suffix_xi_xi_1}')
+    print(f'H_xi(x_i+1) = H(x_i x_i+1) - H(x_i) = {H_xi_xi_1} - {H_xi} = {H_suffix_xi_xi_1}')
+
     return result
 
 
 def main():
     suffix: int = 1
-    # Читаем файл
+    # Чтение файла
     file_input = open(f'./input/input_{suffix}.txt', 'r')
     # Создаём массив непустных строк из файла
     lines = file_input.read().splitlines()
@@ -186,22 +194,37 @@ def main():
     # Добавляем ключ, значение в словарь
     ensemble: dict = dict()
     for i, line in enumerate(lines):
+        # len(line) == 2
         line = line.split(':')
         for word in line:
+            # В случае лишних пробелов
             word.replace(' ', '')
 
+        # Если Вы заметили ошибку в этом блоке,
+        # то у вас неправильно форматирован вход.
+        # Пожалуйста, пишите так:
+        # a|a: 0.5
+        # b|a: 0.5
+        #
+        # a|b: 0.6
+        # b|b: 0.4
         what_alpha = str(line[0].split('|')[1])
         if what_alpha != alphabet[i // math.floor(math.sqrt(len(lines)))]:
             raise ValueError(f'Ищите и исправляйте строку: {line}')
 
+        # a|a, p(a|a)
         key, value = line
         ensemble[key] = np.float64(value)
 
+        # В list_ensembles_conditional_p входят словари длиной sqrt(кол-во строчек).
+        # Весь ввод данных оставляется на совесть пользователя. :)
         if (i + 1) % math.floor(math.sqrt(len(lines))) == 0:
             list_ensembles_conditional_p.append(ensemble)
             ensemble = dict()
 
     # print(list_ensembles_conditional_p)
+
+    # Однако я предусмотрел проверку на валидность сумм вероятностей.
     def test_valid(l_ensemble: list) -> bool:
         for i in range(len(l_ensemble)):
             summary: np.float64 = np.float64(0)
@@ -212,16 +235,20 @@ def main():
                 return False
         return True
 
-    # Сумма вероятностей должна быть равна 1.0
+    # Сумма вероятностей для каждого из p(*|a), p(*|b), ... должна быть равна 1.0
     if test_valid(list_ensembles_conditional_p):
 
+        # Возвращает список словарей:
+        # [{p(x_i = a|x_i+1 = a), p(x_i = a|x_i+1 = b)},
+        # {p(x_i = b|x_i+1 = a), p(x_i = b|x_i+1 = b)}]
         result = markov_chain(list_ensembles_conditional_p)
 
+        # Запись в файл
         file_output = open(f'./output/output_{suffix}.txt', 'w')
         for ensemble in result:
             for key, value in ensemble.items():
                 file_output.write(f'{key}: {value}\n')
-            file_output.write('\n')    
+            file_output.write('\n')
         file_output.close()
 
     else:
